bcbd5b6c7c383bc5ce2ff3a3e7f4a44ca6869d73
Muhammad Hatta Putra <hatta.putraaa@gmail.com>
2022-10-21 20:27:02 +0700

    Update spring security config

--- src/main/java/org/binar/kampusmerdeka/config/CorsFilter.java
+++ src/main/java/org/binar/kampusmerdeka/config/CorsFilter.java
@@ -17,7 +17,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request= (HttpServletRequest) servletRequest;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
 //        response.setHeader("Access-Control-Allow-Credentials", true);
